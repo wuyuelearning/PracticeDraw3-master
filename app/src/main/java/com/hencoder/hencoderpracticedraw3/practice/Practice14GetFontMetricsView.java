538f0be567dd386ad62aa44d27bf03709d2ba69c
wuyue <wuyue@lvmama.com>
2017-11-24 16:40:06 +0800

    提交

---
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
@@ -0,0 +1,63 @@
+package com.hencoder.hencoderpracticedraw3.practice;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+public class Practice14GetFontMetricsView extends View {
+    Paint paint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+    String[] texts = {"A", "a", "J", "j", "Â", "â"};
+    int top = 200;
+    int bottom = 400;
+
+    public Practice14GetFontMetricsView(Context context) {
+        super(context);
+    }
+
+    public Practice14GetFontMetricsView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice14GetFontMetricsView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        paint1.setStyle(Paint.Style.STROKE);
+        paint1.setStrokeWidth(20);
+        paint1.setColor(Color.parseColor("#E91E63"));
+        paint2.setTextSize(160);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawRect(50, top, getWidth() - 50, bottom, paint1);
+
+        // 使用 Paint.getFontMetrics() 计算出文字的显示区域
+        // 然后计算出文字的绘制位置，从而让文字上下居中
+        // 这种居中算法的优点是，可以让不同的文字的 baseline 对齐
+
+        int middle = (top + bottom) / 2;
+        Paint.FontMetrics fontMetrics =paint2.getFontMetrics();
+        float f1 = -(fontMetrics.top+fontMetrics.bottom)/2;
+
+        float f = -(fontMetrics.descent+fontMetrics.ascent)/2;
+        Log.d("+++++++","_"+fontMetrics.top+"_"+fontMetrics.bottom+"_"+ fontMetrics.ascent+"_"+fontMetrics.descent+"_"+fontMetrics.leading+"_"+f);
+
+        canvas.drawText(texts[0], 100, middle+f, paint2);
+//        canvas.drawText(texts[0], 110, middle+f1, paint2);
+        canvas.drawText(texts[1], 200, middle+f, paint2);
+        canvas.drawText(texts[2], 300, middle+f, paint2);
+        canvas.drawText(texts[3], 400, middle+f, paint2);
+        canvas.drawText(texts[4], 500, middle+f, paint2);
+        canvas.drawText(texts[5], 600, middle+f, paint2);
+    }
+}
